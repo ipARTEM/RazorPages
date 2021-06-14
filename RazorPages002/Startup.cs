0749eb68fc13d30ea053eebf812aa143cc3c6e6a
Artem <a.khimin@yandex.ru>
2021-06-14 20:48:20 +0300

    Сделал подключение к серверу

--- RazorPages002/Startup.cs
+++ RazorPages002/Startup.cs
@@ -2,6 +2,7 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Routing;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -25,6 +26,11 @@ namespace RazorPages002
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddDbContextPool<AppDbContext>(options =>
+            {
+                options.UseSqlServer(Configuration.GetConnectionString("EmployeeDBConnection"));
+            });
+
             services.AddSingleton<IEmployeeRepository, MockEmployeeRepositury>();
             services.AddRazorPages();
 
